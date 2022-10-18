a9717e05fbf934c8424d669339e9f82829aafc33
Alan-RiBas <alanbatista.ribas@gmail.com>
2022-10-18 20:35:45 -0300

    criando função para receber api, e função para exibir os items no html

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,61 @@
-import {api} from './api';
+// import { api } from "./api";
+// const api = require('./api');
+const api= {
+  url: 'https://api.openweathermap.org/data/2.5/',
+  key: 'a8d8cd439a45dbdbf2ea1569594eb2aa',
+  lang: 'pt_br',
+  units: 'metric'
+};
+const inputSearch = document.querySelector('.input-search');
+const btnSearch = document.querySelector('.btn-search');
 
-const inputSearch = document.querySelector('#input-search');
-const btnSearch = document.querySelector('#btn-search');
+//campos selecionados para preencher
+const city = document.querySelector('.cidade');
+const currentDate = document.querySelector('.data-atual');
+
 
 //funções
-const searchResults = (city)=>{
-  // fetch()
-  console.log('função dale')
+const apiJson = (city)=>{
+   fetch(`${api.url}weather?q=${city}&lang=${api.lang}&units=${api.units}&appid=${api.key}`)
+  .then(response => {
+      if(!response.ok){
+        throw new Error(`http error: status ${response.status}`)
+      }
+      return response.json();
+   })
+   .catch(error =>{alert(error.message)})
+  .then(response => displayResults(response));
+}
+
+
+//função para inserir informações em tela
+const displayResults = (weather)=>{
+  city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+
+  let date = new Date();
+  currentDate.innerHTML = date;
+
+  
+
 }
 
 //eventos
 btnSearch.addEventListener('click', ()=>{
-  // const city = inputSearch.value;
-  console.log('chegou')
-  // if(!city && city == '' && city == Number(city)){
-  //   return alert('error');
-  // }else{
-  //   // searchResults(city);
-  //   console.log('chegou')
-  // }
+  const city = inputSearch.value;
+  const error = 'cidade inválida';
+  if(!city && city == '' && city == Number(city)){
+    return alert(error);
+
+  }else{
+    apiJson(city);
+  }
 });
+
 inputSearch.addEventListener('keypress',(e)=>{
   const tecla = e.keyCode;
   const city = inputSearch.value;
   if(tecla === 13){
-    console.log('enter');
-  }else{
-    console.log('não é o enter');
+    apiJson(city);
   }
 });
+
